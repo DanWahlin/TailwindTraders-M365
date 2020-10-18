b7af55d3ba4c0256c2b55bbf7a1a0d6585fec5ca
Dan Wahlin <danwahlin@iMac-Office.local>
2020-10-17 19:17:59 -0700

    Initial check-in

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tailwind Traders M365
+
+
+## Running the Application
+
+1. Install the latest LTS version of Node.js from https://nodejs.org. *IMPORTANT: The server uses ES2015 features AND the Angular CLI so you need a current version of Node.js.*
+
+1. Run `npm install` to install app dependencies
+
+1. Run `ng build --watch` to build and bundle the code
+
+1. Run `npm start` in a separate terminal window to build the TypeScript, watch for changes and launch the web server
+
+1. Go to http://localhost:8080 in your browser 
+
+Simply clone the project or download and extract the .zip to get started. 
+
---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit, ViewChild, 
+  ViewContainerRef, ComponentFactoryResolver, ComponentRef } from '@angular/core';
+
+import { DataService } from '../core/services/data.service';
+import { ICustomer, IPagedResults } from '../shared/interfaces';
+import { FilterService } from '../core/services/filter.service';
+import { LoggerService } from '../core/services/logger.service';
+
+
+@Component({
+  selector: 'cm-customers',
+  templateUrl: './customers.component.html'
+})
+export class CustomersComponent implements OnInit {
+
+  title: string;
+  filterText: string;
+  customers: ICustomer[] = [];
+  displayMode: DisplayModeEnum;
+  displayModeEnum = DisplayModeEnum;
+  totalRecords = 0;
+  pageSize = 10;
+  mapComponentRef: ComponentRef<any>;
+  _filteredCustomers: ICustomer[] = [];
+
+  get filteredCustomers() {
+    return this._filteredCustomers;
+  }
+
+  set filteredCustomers(value: ICustomer[]) {
+    this._filteredCustomers = value;
+    this.updateMapComponentDataPoints();
+  }
+
+  @ViewChild('mapsContainer', { read: ViewContainerRef }) 
+  private mapsViewContainerRef: ViewContainerRef;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
+    private dataService: DataService,
+    private filterService: FilterService,
+    private logger: LoggerService) { }
+
+  ngOnInit() {
+    this.title = 'Customers';
+    this.filterText = 'Filter Customers:';
+    this.displayMode = DisplayModeEnum.Card;
+
+    this.getCustomersPage(1);
+  }
+
+  changeDisplayMode(mode: DisplayModeEnum) {
+      this.displayMode = mode;
+  }
+
+  pageChanged(page: number) {
+    this.getCustomersPage(page);
+  }
+
+  getCustomersPage(page: number) {
+    this.dataService.getCustomersPage((page - 1) * this.pageSize, this.pageSize)
+        .subscribe((response: IPagedResults<ICustomer[]>) => {
+          this.customers = this.filteredCustomers = response.results;
+          this.totalRecords = response.totalRecords;
+        },
+        (err: any) => this.logger.log(err),
+        () => this.logger.log('getCustomersPage() retrieved customers for page: ' + page));
+  }
+
+  filterChanged(data: string) {
+    if (data && this.customers) {
+        data = data.toUpperCase();
+        const props = ['firstName', 'lastName', 'city', 'state.name'];
+        this.filteredCustomers = this.filterService.filter<ICustomer>(this.customers, data, props);
+    } else {
+      this.filteredCustomers = this.customers;
+    }
+  }
+
+  async lazyLoadMapComponent() {
+    this.changeDisplayMode(DisplayModeEnum.Map);
+    if (!this.mapsViewContainerRef.length) {
+      // Lazy load MapComponent
+      const { MapComponent } = await import('../shared/map/map.component');
+      console.log('Lazy loaded map component!');
+      const component = this.componentFactoryResolver.resolveComponentFactory(MapComponent);
+      this.mapComponentRef = this.mapsViewContainerRef.createComponent(component);
+      this.mapComponentRef.instance.zoom = 2;
+      this.mapComponentRef.instance.dataPoints = this.filteredCustomers;
+      this.mapComponentRef.instance.enabled = true;
+    }
+  }
+
+  updateMapComponentDataPoints() {
+    if (this.mapComponentRef) {
+      this.mapComponentRef.instance.dataPoints = this.filteredCustomers;
+    }
+  }
+
+
+}
+
+enum DisplayModeEnum {
+  Card = 0,
+  Grid = 1,
+  Map = 2
+}
