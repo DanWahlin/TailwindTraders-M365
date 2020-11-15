e4a0949f5471d8595dbb9421b27c567a1798e1d7
Dan Wahlin <danwahlin@iMac-Office.local>
2020-11-15 01:14:10 -0700

    Added reusable getJson() function for bot

--- Teams/services/customers.js
+++ Teams/services/customers.js
@@ -3,8 +3,8 @@ const fetch = require('node-fetch');
 class CustomerService {
 
     async getLatestCustomer() {
-        const customers = await this.getCustomers();
-        const salesPeople = await this.getSalesPeople();
+        const customers = await this.getJson('/api/customers');
+        const salesPeople = await this.getJson('/api/salespeople');
         if (customers && customers.length) {
             const customer = customers[customers.length - 1];
             if (salesPeople && salesPeople.length) {
@@ -18,61 +18,18 @@ class CustomerService {
         }
     }
 
-    async getCustomers() {
+    async getJson(apiUrl) {
         let result;
         try {
             // Checking port since if ngrok is specific and port is 80 we don't want to add it (will break)
             const port = (process.env.CrmPort === '80') ? '' : `:${process.env.CrmPort}`;
-            const url = `${process.env.CrmUrl}${port}/api/customers`;
+            const url = `${process.env.CrmUrl}${port}${apiUrl}`;
             const response = await fetch(url, {
                 method: "GET",
                 headers: { Accept: "application/json" }
             });
     
-            console.log (`Received customers with status code ${response.status}`);
-    
-            if (response.status !== 200) {
-                throw `Response error ${response.status}: ${response.statusText}`;
-            }
-
-            const result = await response.json();   
-            return result; 
-        }
-        catch (error) {
-            console.log(error);
-            result = 
-            [{
-                "id": 1,
-                "firstName": `Error fetching customer data`,
-                "lastName": "",
-                "gender": "",
-                "address": error,
-                "city": "  ",
-                "state": {
-                    "abbreviation": "",
-                    "name": ""
-                },
-                "orders": [],
-                "latitude": 0,
-                "longitude": 0
-            }];
-        }
-        
-        return result;
-    }
-
-    async getSalesPeople() {
-        let result;
-        try {
-            // Checking port since if ngrok is specific and port is 80 we don't want to add it (will break)
-            const port = (process.env.CrmPort === '80') ? '' : `:${process.env.CrmPort}`;
-            const url = `${process.env.CrmUrl}${port}/api/salespeople`;
-            const response = await fetch(url, {
-                method: "GET",
-                headers: { Accept: "application/json" }
-            });
-    
-            console.log (`Received sales people with status code ${response.status}`);
+            console.log (`Received ${apiUrl} data with status code ${response.status}`);
     
             if (response.status !== 200) {
                 throw `Response error ${response.status}: ${response.statusText}`;
